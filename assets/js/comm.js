cd8b27648275b6d416539c2cf3c7de99dd6d53cc
kimdust <mji1223@naver.com>
2024-01-18 15:11:28 +0900

    gnb 디자인 및 슬라이드 메뉴 완료

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,1 +1,24 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // gnb 메뉴 호버 시
+  $(".gnb_1depth > li").hover(
+    function () {
+      $(this).find(".gnb_2depth").stop().slideDown(300);
+      $(this).children("a").css("color", "#0067ac");
+    },
+    function () {
+      $(this).find(".gnb_2depth").stop().slideUp(300);
+      $(this).children("a").css({
+        color: "#333333",
+      });
+    }
+  );
+
+  $(".gnb_2depth > li").hover(
+    function () {
+      $(this).children("a").css("font-family", "WooridaumR");
+    },
+    function () {
+      $(this).children("a").css("font-family", "WooridaumL");
+    }
+  );
+});
