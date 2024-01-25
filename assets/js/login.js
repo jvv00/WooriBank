be181d67cbc8609b06ce18c362244325965e667e
jvv00 <jvv0006@gmail.com>
2024-01-25 18:20:50 +0900

    login : 로그인 입력 시 버튼 활성화, 클릭 시 초기화 기능

--- assets/js/login.js
+++ assets/js/login.js
@@ -37,19 +37,52 @@ function toArray(nodeList) {
     });
   });
 
-  document.getElementById('IdLogin').addEventListener('click', function (event) {
-    event.preventDefault(); // 이벤트 기본 동작 방지
-
-    // 입력된 값 가져오기
-    var userIdValue = document.getElementById('userId').value;
-    var passwordValue = document.getElementById('password').value;
-
-    // 가져온 값으로 원하는 작업 수행 (예: 서버로 전송, 검증 등)
-    console.log('아이디:', userIdValue);
-    console.log('비밀번호:', passwordValue);
-
-    // 원하는 작업 수행 후 페이지 이동 등 추가 작업이 필요하다면 여기에 추가
-
-});
+  document.addEventListener("DOMContentLoaded", function () {
+    // 로그인 버튼 활성화
+    const userIdInput = document.getElementById('userId');
+    const passwordInput = document.getElementById('password');
+    const btn2 = document.getElementById('IdLoginBtn');
   
-
+    function handleInput() {
+      // input 값이 비어있지 않으면 btn2의 색상을 파란색으로 변경
+      if (userIdInput.value && passwordInput.value) {
+        btn2.classList.remove('disabled');
+      } else {
+        // input 값이 비어있으면 기본 색상으로 변경
+        btn2.classList.add('disabled');
+      }
+    }
+  
+    // 초기 상태에서 input 값이 비어있으면 버튼 비활성화
+    handleInput();
+  
+    // input 값이 변경될 때 이벤트 처리
+    userIdInput.addEventListener('input', handleInput);
+    passwordInput.addEventListener('input', handleInput);
+  
+    // 클릭 이벤트 처리
+    btn2.addEventListener('click', function (event) {
+      if (btn2.classList.contains('disabled')) {
+        event.preventDefault();
+        // 버튼이 비활성화되어 있으면 클릭 이벤트 중단
+      } else {
+        // 버튼이 활성화되어 있으면 클릭 이벤트 처리
+        // 여기에 로그인 등의 동작 추가
+  
+        // 입력된 값 가져오기
+        var userIdValue = userIdInput.value;
+        var passwordValue = passwordInput.value;
+        
+         // 입력된 값 초기화
+        userIdInput.value = '';
+        passwordInput.value = '';
+        // 가져온 값으로 원하는 작업 수행 (예: 서버로 전송, 검증 등)
+        console.log('아이디:', userIdValue);
+        console.log('비밀번호:', passwordValue);
+        
+        btn2.classList.add('disabled');
+        // 원하는 작업 수행 후 페이지 이동 등 추가 작업이 필요하다면 여기에 추가
+      }
+    });
+  });
+  
