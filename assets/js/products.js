c52a6f560f30ef67ae59ac17b00c72603de6827c
yejjjin <ggreat7712@gmail.com>
2024-02-02 18:47:00 +0900

    products : 완성 (디테일 추가 작업 필요)

--- assets/js/products.js
+++ assets/js/products.js
@@ -13,10 +13,26 @@ AOS.init({
 
 // mobile swiper 상단 버튼
 var swiper = new Swiper(".product-menu-m", {
-  slidesPerView: 3,
+  slidesPerView: 4.5,
+});
+// 상단 버튼 클릭시 색상변경
+document.addEventListener("DOMContentLoaded", function () {
+  const menuLinks = document.querySelectorAll(".menu-link");
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      menuLinks.forEach(function (link) {
+        link.classList.remove("clicked");
+      });
+
+      link.classList.add("clicked");
+    });
+  });
 });
 
 //모바일 상품 컨텐츠
 var swiper = new Swiper(".product-contents-m", {
-  slidesPerView: 2.5,
+  slidesPerView: 2,
 });
